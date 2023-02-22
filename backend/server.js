89b5b2b5e6a8db7f516167d66286081003e5865d
braincee <kwesi938@gmail.com>
2023-02-22 20:09:33 +0000

    express setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+require('dotenv').config();
+
+// express app
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+// routes
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to workout'})
+} )
+
+
+//listen for request
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
