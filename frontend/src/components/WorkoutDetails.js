c700ba79274343bf58329006b65719e49b1384bd
braincee <kwesi938@gmail.com>
2023-02-23 13:59:53 +0000

    add  delete function & button to workout details

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
 
-const WorkoutDetails = ({ workout}) => {
+const WorkoutDetails = ({ workout }) => {
+    
+    const { dispatch } = useWorkoutContext()
+    const handleDelete = async () => {
+      const response = await fetch('/api/workouts/' + workout._id, {
+        method: 'DELETE'
+      })  
+
+      const json = await response.json()
+
+      if(response.ok) {
+        dispatch({type: 'DELETE_WORKOUT', payload: json})
+      }
+    }
+
   return (
     <div>
         <div className="workout-details">
@@ -8,6 +23,7 @@ const WorkoutDetails = ({ workout}) => {
             <p>Load in (kg): <strong>{workout.load}</strong></p>
             <p>Reps: <strong>{workout.reps}</strong></p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleDelete}>delete</span>
         </div>
     </div>
   )
