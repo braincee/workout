ac3171d9e52dd6ff3b25343e03a66da6e7dba017
braincee <kwesi938@gmail.com>
2023-02-23 22:35:23 +0000

    Add using login logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -47,4 +47,29 @@ userSchema.statics.signup = async function (email, password){
     return user
 }
 
+
+// static login method
+
+userSchema.statics.login = async function(email, password) {
+  
+    // validation 
+    if(!email || !password) {
+        throw Error('All fields must be field')
+    }
+    
+    const user = await this.findOne({ email })
+
+    if(!user) {
+        throw Error('Incorrect Email')
+    }
+    
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
