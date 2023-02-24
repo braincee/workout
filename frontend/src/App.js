6e87dc59d15c96506db57ddb11b93859a36a040b
braincee <kwesi938@gmail.com>
2023-02-24 10:46:16 +0000

    Add user login, logout and signup logic to the nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 import Main from './pages/Main';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
+
+  const { user } = useAuthContext()
+
   return (
     <div>
      <Router>
       <Navbar />
       <div className='pages'>
       <Routes>
-        <Route exact path="/" element={<Main />}/>
+        <Route exact path="/" element={user ? <Main /> : <Navigate to="/login"/>} />
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
       </Routes>
       </div>
      </Router>
